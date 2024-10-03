135927bc65b20681a6f1ed4ba85a6d2dc6465e3b
Mohamed Abdelhalim <95293830+1abdelhalim@users.noreply.github.com>
2024-10-03 15:29:28 +0300

    Update spark_app.py

--- spark_app.py
+++ spark_app.py
@@ -3,7 +3,7 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
-# Create Spark session
+#  Spark session
 spark = SparkSession.builder \
     .appName("PredictiveMaintenance") \
     .config("spark.mongodb.input.uri", "mongodb://mongodb1:27017/sensor_data") \
@@ -13,7 +13,6 @@ spark = SparkSession.builder \
 # Load data from MongoDB
 df = spark.read.format("mongo").load()
 
-# Feature engineering
 assembler = VectorAssembler(inputCols=[
     "Air temperature", "Process temperature", "Rotational speed",
     "Torque", "Tool wear"
@@ -21,17 +20,13 @@ assembler = VectorAssembler(inputCols=[
 
 data = assembler.transform(df)
 
-# Train-test split
 train, test = data.randomSplit([0.8, 0.2])
 
-# Random Forest Classifier
 rf = RandomForestClassifier(labelCol="Machine failure", featuresCol="features", numTrees=10)
 model = rf.fit(train)
 
-# Prediction
 predictions = model.transform(test)
 
-# Evaluation
 evaluator = MulticlassClassificationEvaluator(labelCol="Machine failure", metricName="accuracy")
 accuracy = evaluator.evaluate(predictions)
 
